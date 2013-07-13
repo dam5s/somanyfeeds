114b9014be9fd60a1561117ffe4ac7e144c90d2c
Damien Le Berrigaud <damien@itsdamo.com>
2013-07-13 18:30:46 +0000

    Added masonry for layouting

--- public/js/aggregator/articles_view.js
+++ public/js/aggregator/articles_view.js
@@ -3,16 +3,32 @@ var SMF = SMF || {};
 SMF.ArticlesView = function() {
   this.$el = $('#articles');
   this.template = $('#article_tmpl').html();
+  this.masonry = new Masonry(document.querySelector("#articles"), {itemSelecotr: "article"});
+  this.articleElements = null;
 };
 
 SMF.ArticlesView.prototype = {
   render: function() {
     var self = this;
-    this.$el.html('');
+
+    var $articles = $("article", self.$el);
+    if ($articles.length > 0) {
+      this.masonry.remove($articles);
+      $articles.remove();
+    }
 
     _(this.articles).each(function(article) {
       var renderedArticle = _.template(self.template, article);
       self.$el.append(renderedArticle);
+    });
+
+    var $articles = $("article", self.$el);
+
+    this.$el.imagesLoaded(function() {
+      setTimeout(function() {
+        self.masonry.appended($articles);
+        self.masonry.layout();
+      }, 800);
     });
   }
 };
